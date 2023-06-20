9052dddbf5eb54dde317a65c5e92f358b59acfd0
Mauricio Diaz <mauricio.diaz@inria.fr>
2023-06-20 15:36:09 +0200

    Add command for downloading data in demo DL container

--- demo-dl-project/init-demo-dl-project.sh
+++ demo-dl-project/init-demo-dl-project.sh
@@ -10,5 +10,10 @@ SOURCE_DIR=${WORK_DIR}/code
 REPO_URL=https://gitlab.inria.fr/mdiazmel/dl-example-project.git
 git clone --depth 1 $REPO_URL $SOURCE_DIR
 
+# Download data
+cd $SOURCE_DIR
+wget https://gitlab.inria.fr/mdiazmel/dl-example-project/-/raw/main/data_demo_dl.tar.gz
+tar xf data_demo_dl.tar.gz
+
 # Give write permissions
 chown -R onyxia:users $SOURCE_DIR/
